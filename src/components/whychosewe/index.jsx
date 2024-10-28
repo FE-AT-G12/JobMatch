920c7228122485650f7f09109676a8b97c69bf0b
hainguyen130103 <hai263672@gmail.com>
2024-10-28 15:57:33 +0700

    home demo2

---
+++ src/components/whychosewe/index.jsx
@@ -0,0 +1,32 @@
+import "./index.scss";
+function Whychose() {
+  return (
+    <div className="whychose">
+      <h1>VÌ SAO CHỌN JOB MATCH?</h1>
+      <p>
+        Job Match là nền tảng tiên phong kết nối các freelancer đa độ tuổi với
+        những công việc nhỏ, nhanh và linh hoạt. Với mục tiêu hiểu rằng nhu cầu
+        tìm kiếm công việc tạm thời, ngắn hạn và dễ thực hiện đang ngày càng
+        tăng, đặc biệt là trong các lĩnh vực như tạp vụ, phục vụ, lễ tân, và
+        nhiều công việc khác không yêu cầu chuyên môn cao.
+      </p>
+      <p>
+        Nơi kết nối các freelancer với những công việc online linh hoạt, phù hợp
+        với mọi độ tuổi và kỹ năng. Với việc hiểu rằng nhu cầu làm việc từ xa,
+        đặc biệt trong các công việc như thiết kế slide, thuyết trình, quản lý
+        dự án, nhập liệu, và nhiều công việc khác đang ngày càng gia tăng. Nền
+        Tảng này là giải pháp giúp bạn tìm kiếm cơ hội việc làm trực tuyến dễ
+        dàng, đáp ứng nhu cầu làm việc tại nhà hay ở bất kỳ đâu.
+      </p>
+      <p>
+        Với nền tảng này, các freelancer thuộc mọi độ tuổi – từ sinh viên đến
+        người cao tuổi – đều có cơ hội tìm kiếm những công việc phù hợp với thời
+        gian và kỹ năng của mình. Chỉ với vài thao tác đơn giản, bạn có thể kết
+        nối ngay với các nhà tuyển dụng, các cửa hàng cần nhân lực nhanh chóng,
+        dễ dàng tìm việc trong khu vực gần bạn và kiếm thu nhập linh hoạt.
+      </p>
+    </div>
+  );
+}
+
+export default Whychose;
