16dee33791c8c0e323c618f83a83fc3ebdf88678
PhamVietHoang <hoangpvse183123@fpt.edu.vn>
2024-10-30 21:39:24 +0700

    Use prettier

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -1,13 +1,13 @@
-import UserTest from "../page/User/UserTest";
-import LoginPage from "../page/login";
-import RegisterPage from "../page/register";
-import Layout from "../components/layout";
-import HomePage from "../page/home";
-import Job from "../page/Job/Job";
-import adminAccount from "../page/adminAccount/adminAccount";
-import HomePage from "../page/home";
-import Job from "../page/Job/Job";
-import Company from "../page/company";
+import UserTest from '../page/User/UserTest'
+import LoginPage from '../page/login'
+import RegisterPage from '../page/register'
+import Layout from '../components/layout'
+import HomePage from '../page/home'
+import Job from '../page/Job/Job'
+import adminAccount from '../page/adminAccount/adminAccount'
+import HomePage from '../page/home'
+import Job from '../page/Job/Job'
+import Company from '../page/company'
 import UserTest from '../page/User/UserTest'
 import LoginPage from '../page/login'
 import RegisterPage from '../page/register'
@@ -21,7 +21,7 @@ const routes = [
     layout: MainLayout,
     data: [
       {
-        path: "/",
+        path: '/',
         isIndex: true,
         component: HomePage,
       },
@@ -34,11 +34,11 @@ const routes = [
         component: RegisterPage,
       },
       {
-        path: "/user",
+        path: '/user',
         component: UserTest,
       },
       {
-        path: "/job",
+        path: '/job',
         component: Job,
       },
       {
@@ -46,14 +46,14 @@ const routes = [
         component: adminAccount,
       },
       {
-         path: '/jobAdmin',
+        path: '/jobAdmin',
         component: JobAdmin,
       },
       {
         path: '/jobAdmin/:jobId',
         component: JobAdminDetail,
       },
-    ]
+    ],
   },
 ]
 
