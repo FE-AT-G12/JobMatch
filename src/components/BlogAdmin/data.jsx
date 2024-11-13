bccb3163b07f0e28db8cff2d3e2b5337b581f7b6
PhamVietHoang <hoangpvse183123@fpt.edu.vn>
2024-11-13 22:26:21 +0700

    Add blog feat

---
+++ src/components/BlogAdmin/data.jsx
@@ -0,0 +1,46 @@
+import { Button } from 'antd'
+import { Link } from 'react-router-dom'
+import { BlogDetailButton } from './BlogDetailButton'
+export const collumn = [
+  {
+    title: 'Số thứ tự',
+    dataIndex: 'index',
+    key: 'index',
+  },
+  {
+    title: 'Tiêu đề',
+    dataIndex: 'title',
+    key: 'title',
+  },
+  {
+    title: 'Mô tả',
+    dataIndex: 'description',
+    key: 'description',
+  },
+  {
+    title: 'Image',
+    dataIndex: 'thumbnail',
+    key: 'image',
+    render: (thumbnail) => (
+      <img src={thumbnail} alt='thumbnail' style={{ width: '50px' }} />
+    ),
+  },
+  {
+    title: 'Author',
+    dataIndex: 'author',
+    key: 'author',
+  },
+  {
+    title: 'Date posted',
+    dataIndex: 'datePosted',
+    key: 'date',
+  },
+  {
+    title: 'Action',
+    dataIndex: 'action',
+    key: 'action',
+    render: (text, record) => (
+      <BlogDetailButton record={record}>Chi tiết</BlogDetailButton>
+    ),
+  },
+]
